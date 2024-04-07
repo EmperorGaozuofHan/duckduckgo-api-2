32d9fdea5a546010d7eb7c8289933e64305da3b5
Emperor Gaozu of Han <163540265+EmperorGaozuofHan@users.noreply.github.com>
2024-04-07 22:21:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request
-from api.index import search
+from api.index import search, search_internet
 app = Flask(__name__)
 
-
 app.route('/search')(search)
+app.route('/search_internet', methods=['POST'])(search_internet)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
+
